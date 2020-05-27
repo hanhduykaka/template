96b11a4cb90c729db0cc0aed7329175dfe9a89d1
taittn <taittn@hitachiconsulting.com>
2020-05-27 15:24:29 +0700

    first commit

---
+++ src/components/components/components.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ShopComponent } from "./shop/shop.component";
+import { CartComponent } from "./cart/cart.component";
+
+@NgModule({
+  declarations: [ShopComponent, CartComponent],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    ShopComponent,
+  ],
+})
+export class ComponentsModule {}
